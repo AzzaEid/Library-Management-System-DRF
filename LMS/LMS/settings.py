b9df1b5a4f445d94d1b0001bd224b7ab3ab4c6dc
Azza Eid <azzaabed2003@gmail.com>
2025-10-19 19:44:11 +0300

    register app

--- LMS/LMS/settings.py
+++ LMS/LMS/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'library_app',
+    'rest_framework',
+
 ]
 
 MIDDLEWARE = [
